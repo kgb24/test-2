bab4f2ee07ac4fb74a159d6c4e91e7b1de73104f
Black Chen <chenzhaokun@rocketmail.com>
2023-06-12 17:55:48 +0800

    my project push

---
+++ IF.learnig.py
@@ -0,0 +1,17 @@
+In [8]: if 3<4 :
+   ...:    print ('3<4')
+   ...: elif 4 != 4:
+   ...:    print ('4==4')
+   ...: else:
+   ...:    print ('else')
+   ...:
+3<4
+
+In [9]: if 3>4 :
+   ...:    print ('3>4')
+   ...: elif 4 != 4:
+   ...:    print ('4==4')
+   ...: else:
+   ...:    print ('else')
+   ...:
+else
